26490cefb7102772ae31638c23db74c7c2086c5b
RidwanC134 <ridwanhh10@gmail.com>
2023-04-03 10:56:08 +0700

    Membuat struct struktur di dalam struktur

---
+++ ActivityStructure2_134/ActivityStructure2_134.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+struct AlamatDetail 
+{
+    string desa;
+    string kota;
+};
+
+struct Mahasiswa
+{
+    string NIM;
+    string nama;
+    AlamatDetail alamat;
+    int umur;
+};
+
+
+int main() {
+    Mahasiswa mhs;
+
+    cout << "Masukan NIM :";
+    cin >> mhs.NIM;
+    cout << "Masukan Nama :";
+    cin >> mhs.nama;
+    cout << "Alamat :";
+    cout << "\n\tMasukan Desa :";
+    cin >> mhs.alamat.desa;
+    cout << "\tMasukan Kota :";
+    cin >> mhs.alamat.kota;
+    cout << "Masukan Umur : ";
+    cin >> mhs.umur;
+
+    cout << "\nNIM = " << mhs.NIM;
+    cout << "\nNama = " << mhs.nama;
+    cout << "\nAlamat = " << mhs.alamat.desa;
+    cout << "\nAlamat = " << mhs.alamat.kota;
+    cout << "\nUmur = " << mhs.umur;
+}
+
