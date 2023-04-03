777d2a82750d1f5f5e9df79885aa14d15d4748a7
RidwanC134 <ridwanhh10@gmail.com>
2023-04-03 10:28:13 +0700

    Membuat iostream

--- Activity_Structure_134/Activity_Structure_134.cpp
+++ Activity_Structure_134/Activity_Structure_134.cpp
@@ -1,7 +1,40 @@
-#include <iostream>
+#include <iostream> 
+using namespace std;
 
-int main()
+
+struct mahasiswa
 {
-    std::cout << "Hello World!\n";
+    string NIM;
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main() {
+    mahasiswa mhs1, mhs2;
+
+    mhs1.NIM = "20210140019";
+    mhs1.nama = "Ridwan Hidayatullah";
+    mhs1.alamat = "Sukabumi";
+    mhs1.umur = 19;
+
+    cout << "Masukan NIM :";
+    cin >> mhs2.NIM;
+    cout << "Masukan Nama :";
+    cin >> mhs2.nama;
+    cout << "Masukan Alamat :";
+    cin >> mhs2.alamat;
+    cout << "Masukan Umur: ";
+    cin >> mhs2.umur;
+
+    cout << "\nNIM = " << mhs1.NIM;
+    cout << "\nNama = " << mhs1.nama;
+    cout << "\nAlamat = " << mhs1.alamat;
+    cout << "\nUmur = " << mhs1.umur;
+
+    cout << "\n\nNIM = " << mhs2.NIM;
+    cout << "\nNama = " << mhs2.nama;
+    cout << "\nAlamat = " << mhs2.alamat;
+    cout << "\nUmur = " << mhs2.umur;
 }
 
